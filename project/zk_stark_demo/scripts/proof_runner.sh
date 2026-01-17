03cf25f8101517ede709a00ce0c43d94aadcb83f
viktorashi <ioanvictorstan@gmail.com>
2026-01-17 23:58:14 +0200

    added runner scripts

---
+++ project/zk_stark_demo/scripts/proof_runner.sh
@@ -0,0 +1,1 @@
+uv run ../src/zk_stark_demo/cli/prover_cli.py --length 8 --output proof.json
---
+++ project/zk_stark_demo/scripts/verifier_runner.sh
@@ -0,0 +1,1 @@
+uv run ../src/zk_stark_demo/cli/verifier_cli.py --proof proof.json --length 8 --result 34
